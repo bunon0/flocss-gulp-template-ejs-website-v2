55ab8f0b899222267044cd3ce59f1469d8f5c17d
bunon0 <86172592+bunon0@users.noreply.github.com>
2022-09-28 20:34:31 +0900

    #5 prettierignoreが効かない問題を修正

--- dev/.stylelintrc.js
+++ dev/.stylelintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: ["stylelint-scss"],
-  extends: ["stylelint-config-recommended-scss", "stylelint-prettier/recommended"],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recommended-scss",
+    "stylelint-prettier/recommended",
+  ],
   ignoreFiles: ["node_modules/**", "dist/**/*.css", "assets/css/libs/*.css"],
   rules: {
     "no-descending-specificity": null,
